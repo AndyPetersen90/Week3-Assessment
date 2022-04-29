905746e3000b4811d3c9706c766f7244a6b158b5
Andrew Petersen <fespetersen1990@gmail.com>
2022-04-29 10:14:45 -0600

    Part 2 completed

--- about.js
+++ about.js
@@ -5,10 +5,20 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Your message has been sent! Thank you for contacting us.')
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+
+let photo = document.querySelector('.rubber-duck')
+
+const photoMessage = () => {
+	alert('I see you are looking at my rubber ducky, have you looked at your own beatiful reflection today?')
+}
+
+photo.addEventListener('mouseover', photoMessage);
